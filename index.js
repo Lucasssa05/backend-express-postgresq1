0514aa1e0c9c032c1894b5c9de088ceb6cbf977d
Lucasssa05 <lucas.s.almeida@aln.senaicimatec.edu.br>
2025-05-18 16:11:54 -0300

    feat: add cors

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
-import express from 'express'
-import router from './src/routes/routes.js'
-
-import sequelize from './src/banco/banco.js';
-import User from './src/models/usuario.js';
-import Contato from './src/models/contato.js';
-import RelationTables from './src/models/relacionamentos.js';
-
+import express from "express";
+import router from "./src/routes/routes.js";
+import cors from "cors";
+import sequelize from "./src/banco/banco.js";
+import User from "./src/models/usuario.js";
+import Contato from "./src/models/contato.js";
+import RelationTables from "./src/models/relacionamentos.js";
 
 (async () => {
   try {
     await sequelize.authenticate();
-    console.log('Conectado com sucesso ao PostgreSQL da Vercel/Neon!');
+    console.log("Conectado com sucesso ao PostgreSQL da Vercel/Neon!");
 
-    await sequelize.sync(); 
-    console.log('Modelos sincronizados.');
-
+    await sequelize.sync();
+    console.log("Modelos sincronizados.");
   } catch (error) {
-    console.error('Erro na conexão com o banco: ', error);
+    console.error("Erro na conexão com o banco: ", error);
   }
 })();
 
 const app = express();
 
-
-
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
-app.listen(8080)
+app.listen(8080);
