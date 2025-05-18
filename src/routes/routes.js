f9e9d13353af3fd87adb4bddb8eafa20f69e0f09
Lucasssa05 <lucas.s.almeida@aln.senaicimatec.edu.br>
2025-05-18 16:58:17 -0300

    Update routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,17 +1,28 @@
-import express from 'express';
-import userControle from '../controller/user-controlle.js';
-import verificadorToken from '../middleware/verificador-token.js';
-import protectedController from '../controller/protected-controller.js';
-import contatoController from '../controller/contato-controller.js';
+import express from "express";
+import userControle from "../controller/user-controlle.js";
+import verificadorToken from "../middleware/verificador-token.js";
+import protectedController from "../controller/protected-controller.js";
+import contatoController from "../controller/contato-controller.js";
 const router = express.Router();
 
-router.post('/register', userControle.salvarBanco);
-router.post('/login', userControle.loginBanco);
-router.get('/protected', verificadorToken, protectedController.defesa);
-router.post('/contato/:id', verificadorToken, contatoController.criarContato);
-router.get('/contato/:id', verificadorToken, contatoController.listarContatos);
-router.put('/contato/:id', verificadorToken, contatoController.atualizarContato);
-router.delete('/contato/:id', verificadorToken, contatoController.deletarContato);
-router.patch('/contato/:id', verificadorToken, contatoController.atualizarContatoPatch);
+router.post("/register", userControle.salvarBanco);
+router.post("/login", userControle.loginBanco);
+router.get("/protected", verificadorToken, protectedController.defesa);
+router.post("/contato/:id", verificadorToken, contatoController.criarContato);
+router.get("/contato", verificadorToken, contatoController.listarContatos);
+router.put(
+  "/contato/:id",
+  verificadorToken,
+  contatoController.atualizarContato
+);
+router.delete(
+  "/contato/:id",
+  verificadorToken,
+  contatoController.deletarContato
+);
+router.patch(
+  "/contato/:id",
+  verificadorToken,
+  contatoController.atualizarContatoPatch
+);
 export default router;
-
