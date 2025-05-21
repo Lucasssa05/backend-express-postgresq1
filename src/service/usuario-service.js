2f89f44399fbdb01bbf88da78af39dd11fb891ff
Lucasssa05 <lucas.s.almeida@aln.senaicimatec.edu.br>
2025-05-21 20:56:10 -0300

    Update usuario-service.js

--- src/service/usuario-service.js
+++ src/service/usuario-service.js
@@ -1,59 +1,79 @@
-import bcrypt from 'bcrypt';
-import User from '../models/usuario.js'
-import 'dotenv/config'
-import jwt from 'jsonwebtoken';
-
+import bcrypt from "bcrypt";
+import User from "../models/usuario.js";
+import "dotenv/config";
+import jwt from "jsonwebtoken";
 
 const registrar = async (req, res) => {
-    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    
-    const {name, password, email} = req.body;
-    console.log(name, password, email)
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-    const existingUser = await User.findOne({ where: {email} });
-    if (existingUser) {
-        return res.status(400).json({ message: "E-mail já cadastrado" });
-    }
+  const { name, password, email } = req.body;
+  console.log(name, password, email);
 
-    const salt = await bcrypt.genSalt(10);
-    const hashPassword = await bcrypt.hash(password, salt);
+  const existingUser = await User.findOne({ where: { email } });
+  if (existingUser) {
+    return res.status(400).json({ message: "E-mail já cadastrado" });
+  }
 
-    if (!emailRegex.test(email)) {
-        return res.status(400).json({ message: "E-mail inválido" });
-    }
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(password, salt);
 
-    try{
-        const registeredUser = await User.create({
-            name,
-            password: hashPassword,
-            email
-        });
-        return res.status(201).json({message: "Usuário criado!", user: registeredUser})
-    }catch(error){
-        return res.status(500).json({message: "Erro ao registrar usuário "+error})
-    }
-}
+  if (!emailRegex.test(email)) {
+    return res.status(400).json({ message: "E-mail inválido" });
+  }
+  if (!isPasswordValid(password)) {
+    return res
+      .status(400)
+      .json({
+        message:
+          "A senha deve ter no mínimo 8 caracteres, uma letra maiúscula e um caractere especial.",
+      });
+  }
+  try {
+    const registeredUser = await User.create({
+      name,
+      password: hashPassword,
+      email,
+    });
+    return res
+      .status(201)
+      .json({ message: "Usuário criado!", user: registeredUser });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Erro ao registrar usuário " + error });
+  }
+};
 
 const login = async (req, res) => {
+  const { email, password } = req.body;
+  if (!isPasswordValid(password)) {
+    return res
+      .status(400)
+      .json({
+        message:
+          "A senha deve ter no mínimo 8 caracteres, uma letra maiúscula e um caractere especial.",
+      });
+  }
+  try {
+    console.log(email, password);
+    const findUser = await User.findOne({ where: { email } });
+    console.log(findUser);
+    if (!findUser)
+      return res.status(400).json({ message: "Usuário não achado!" });
 
-    const { email, password } = req.body
+    const match = await bcrypt.compare(password, findUser.password);
 
-    try{
-        console.log(email, password)
-        const findUser = await User.findOne({ where: {email} });
-        console.log(findUser)
-        if(!findUser) return res.status(400).json({message: "Usuário não achado!"})
-        
-        const match = await bcrypt.compare(password, findUser.password)
+    if (!match)
+      return res.status(400).json({ message: "Credenciais inválidas!" });
+    const token = jwt.sign({ id: findUser.id }, process.env.JWT_SECRET, {
+      expiresIn: "1hr",
+    });
+    return res
+      .status(200)
+      .json({ message: "Usuário logado com sucesso!", token });
+  } catch (error) {
+    return res.status(400).json({ message: "Erro ao efetuar login " + error });
+  }
+};
 
-        if(!match) return res.status(400).json({message: "Credenciais inválidas!"})
-        const token = jwt.sign({id: findUser.id}, process.env.JWT_SECRET, {expiresIn: '1hr'})
-        return res.status(200).json({message: "Usuário logado com sucesso!", token})
-        
-    }catch(error){
-        return res.status(400).json({message: "Erro ao efetuar login "+error})
-    }
-
-}
-
-export default {registrar, login}
+export default { registrar, login };
